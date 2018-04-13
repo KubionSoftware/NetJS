1f9840569a67b1530ea529f4bd57db993bcdbcbe
Bouke Stam <bouke.stam@gmail.com>
2018-04-13 10:04:22 +0200

    Added support for command line parameters when running from console

--- Console/Program.cs
+++ Console/Program.cs
@@ -7,6 +7,19 @@ using System.Threading.Tasks;
 namespace NetJS.Console {
     class Program {
         static void Main(string[] args) {
+            var data = "{";
+            for (var i = 1; i < args.Length - 1; i++) {
+                if (i > 1) data += ",";
+
+                if (!args[i].StartsWith("-")) {
+                    System.Console.WriteLine("The parameter name should start with -");
+                    System.Console.ReadLine();
+                    return;
+                }
+                data += args[i].Replace("-", "") + ": \"" + args[i + 1] + "\"";
+            }
+            data += "}";
+
             var application = new JSApplication();
             var service = new JSService();
 
@@ -14,7 +27,7 @@ namespace NetJS.Console {
 
             while (true) {
                 try {
-                    var result = service.RunTemplate("main.js");
+                    var result = service.RunTemplate("main.js", data);
                     System.Console.WriteLine(result);
                 } catch (Exception e) {
                     System.Console.WriteLine(e);
