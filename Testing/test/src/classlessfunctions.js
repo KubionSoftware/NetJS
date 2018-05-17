52c88c5abd537351d9b5fbcd8fd69c2e5b4cbfd8
mrvek <mitchell.v.ek@xs4all.nl>
2018-05-17 13:51:59 +0200

    fixed few tests and and clean coding

--- Testing/test/src/classlessfunctions.js
+++ Testing/test/src/classlessfunctions.js
@@ -1,6 +1,21 @@
 // classlessfunctions include
 assert(() => include("text.txt") == undefined, "include");
+
+//load
 assert(() => load("text.txt") == "text", "load");
+
+// out
 assert(() => out("text") == undefined, "out");
+
+// outLine
 assert(() => outLine("text") == undefined, "outLine");
+
+// import
 assert(() => import("text.txt") == undefined, "import");
+
+// redirect
+
+//unsafe
+var a = false;
+assert(() => unsafe(function () {a = true}) == undefined, "unsafe");
+assert(() => a == true, "unsafe result");
